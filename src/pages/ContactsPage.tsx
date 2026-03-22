f3e8ce563463d70387408777016c19e28977cd94
DiGIMAJ <digimajbusinessenterprise@gmail.com>
2026-03-22 10:47:53 +0100

    feat: add Hollatags SMS button to ContactsPage

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,12 +1,18 @@
 // ... (existing ContactsPage code)
-// Add "Send to Contacts" button:
-<Button
-  onClick={() => {
-    const vcfBlob = new Blob([vcfContent], { type: "text/vcard" });
-    const vcfUrl = URL.createObjectURL(vcfBlob);
-    window.open(vcfUrl, "_blank");
-  }}
-  className="bg-purple-500 hover:bg-purple-600"
->
-  Open in Contacts App
-</Button>
+import { HollatagsButton } from "../components/HollatagsButton";
+
+// ... (rest of ContactsPage code)
+
+<div className="flex gap-2">
+  <Button
+    onClick={() => {
+      const vcfBlob = new Blob([vcfContent], { type: "text/vcard" });
+      const vcfUrl = URL.createObjectURL(vcfBlob);
+      window.open(vcfUrl, "_blank");
+    }}
+    className="bg-purple-500 hover:bg-purple-600"
+  >
+    Open in Contacts App
+  </Button>
+  <HollatagsButton phoneNumbers={selectedContacts.map(c => c.phone)} />
+</div>
